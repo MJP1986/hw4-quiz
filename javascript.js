e4d24799e4e2c4d973ba5c7b3f990ce1da6c10ee
MinJin Park <parkmin1986@gmail.com>
2020-01-03 18:01:24 -0500

    fixed buttons and trying to clear choices

--- javascript.js
+++ javascript.js
@@ -1,19 +1,22 @@
 var start = document.getElementById("start");
 var quiz = document.getElementById("quiz");
 var home = document.getElementById("home");
+var timer = document.getElementById("timer")
 var question = document.getElementById("question");
 var choiceA = document.getElementById("A");
 var choiceB = document.getElementById("B");
 var choiceC = document.getElementById("C");
 var choiceD = document.getElementById("D");
 var result = document.getElementById("comment");
-var buttons = document.querySelector(".multi");
+var buttons = document.querySelector("#choices");
 var score = 0;
 var questionCounter = 0;
 
 document.getElementById("start").onclick = function() { 
   document.getElementById("home").style.display = "none";
+  buttons.style.display = "block";
   renderQuestion();
+
 
 };
 
@@ -23,15 +26,18 @@ function renderQuestion() {
     choiceB.textContent = questions[questionCounter].choices[1];
     choiceC.textContent = questions[questionCounter].choices[2];
     choiceD.textContent = questions[questionCounter].choices[3];
-    // var pl = document.createElement("p");
-    // pl.innerText = questions[questionCounter].title;
-    // question.appendChild(pl);
-    // questionCounter++
-    question.innerText = questions[questionCounter].title;
-    questionCounter++;
+    var pl = document.createElement("p");
+    pl.innerText = questions[questionCounter].title;
+    question.appendChild(pl);
+    questionCounter++
+  
+    if (questions.length === questionCounter) {
+      choices.style.display = "none";
+  console.log("test")
+    }
 };
 
-buttons.addEventListener("click", function() {
+buttons.addEventListener("click", function() {  
   renderQuestion();
 });
 
